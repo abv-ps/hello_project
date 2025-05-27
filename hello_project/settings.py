681b316d4dc40b308b959826bb50b11ba3259a9c
Bogdan <burkalo@gmail.com>
2025-05-27 12:45:07 +0000

    Re-add settings.py without secret key

--- hello_project/settings.py
+++ hello_project/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+from os import getenv
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$ikqoxz#r^)@1v*gm0gs2ew4ddco5p@1p!s#@)8blky+4e0&j3'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", False) == "True"
 
 ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS += ["localhost", "127.0.0.1"]
+    ALLOWED_HOSTS += ["0.0.0.0"]
+    ALLOWED_HOSTS += ["host.docker.internal"]
 
 
 # Application definition
